8f20d820cca50f705dc95ea8e9287f1a330d496d
aldeng1 <143352565+aldeng1@users.noreply.github.com>
2024-01-19 17:00:57 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#define DO int
+#define NOT main
+#define even (
+#define think )
+#define of {
+#define typing :
+#define a std::cout<<
+#define keyword "NO\n";
+#define EVER start;
+#define in return
+#define any 0;
+#define code }
+
+// The line below runs the code
+DO NOT even think of start typing a keyword goto EVER in any code
